a176e3ea18e4a6caea11049f282c66de7421b22f
OneCloudService <112274194+OneCloudService@users.noreply.github.com>
2022-08-27 16:03:30 -0300

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -15,7 +15,4 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
 echo User: administrator
-echo Pass: THTH-channel
-echo View more: https://youtube.com/c/tinhocthuchanh
-echo Visit Create VPS-RDP Part1: https://youtu.be/VnEJv49oKR8 , Part2: https://youtu.be/em221J1k520
 ping -n 10 127.0.0.1 >nul
